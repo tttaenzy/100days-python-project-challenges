808e70e44168b333b6e7670679a6a10bd7b026f8
tenzin choeying <62764541+tttaenzy@users.noreply.github.com>
2023-01-04 16:09:37 +0530

    Update scoreboard.py

--- snake-game-project/scoreboard.py
+++ snake-game-project/scoreboard.py
@@ -6,16 +6,29 @@ class Scoreboard(Turtle):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.score=0
+        self.score = 0
+        with open('data.txt') as file:
+            self.high_score = int(file.read())
         self.color("white")
-        self.setposition(0,280)
-        self.write(f"Score: {self.score}", align="center", font=("Cooper Black", 15, "italic"))
+        self.setposition(0, 280)
+        self.update_score()
 
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}    High Score: {self.high_score}", align="center",
+                   font=("Cooper Black", 15, "italic"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', mode='w') as file:
+                file.write(f'{self.high_score}')
+        self.score = 0
+        self.update_score()
 
     def increase_score(self):
-        self.clear()
-        self.score+=1
-        self.write(f"Score: {self.score}", align="center", font=("Cooper Black", 15, "italic"))
+        self.score += 1
+        self.update_score()
 
 
 class Gameover(Turtle):
@@ -26,6 +39,3 @@ class Gameover(Turtle):
         self.clear()
         self.color("white")
         self.write(f"Game Over", align="center", font=("Cooper Black", 15, "italic"))
-
-
-
