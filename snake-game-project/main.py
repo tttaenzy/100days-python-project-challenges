c073807b9b7596efce7ee510cbcb04146cf0a59e
tenzin choeying <62764541+tttaenzy@users.noreply.github.com>
2023-01-04 16:14:31 +0530

    Update main.py
    there is few changes in the code. i put new changes  which is to reset the game so there is no need to stop game playing. there is also interesting changes which is that 
    whenever you are able to get highscore . that highscore number would be save in file called 'data.txt'. there for when you run snake game time. your highest score will be already there. so you dont need mantained your highscore from zero
    Thank you

--- snake-game-project/main.py
+++ snake-game-project/main.py
@@ -11,41 +11,45 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-food=Food()
-score=Scoreboard()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-#     detect collition with food
+    #     detect collition with food
 
     if snake.snake_segment[0].distance(food) < 15:
         snake.extend()
         food.refresh()
         score.increase_score()
     # detect collition with border
-    elif snake.snake_segment[0].xcor()>280 or snake.snake_segment[0].xcor()< -280 or snake.snake_segment[0].ycor()>280 or snake.snake_segment[0].ycor()< -280:
-        game_is_on=False
-        gameover=Gameover()
-#     detect collition with tail or body of snake
+    elif snake.snake_segment[0].xcor() > 280 or snake.snake_segment[0].xcor() < -280 or snake.snake_segment[
+        0].ycor() > 280 or snake.snake_segment[0].ycor() < -280:
+        # game_is_on=False
+        # gameover=Gameover()
+        score.reset()
+        snake.reset()
+
+    #     detect collition with tail or body of snake
     for segment in snake.snake_segment[1:]:
         # if segment==snake.snake_segment[0]:
         #     pass
         # elif snake.snake_segment[0].distance(segment)<10:
         #     game_is_on=False
         #     gameover=Gameover()
-        if snake.snake_segment[0].distance(segment)<10:
-            game_is_on=False
-            gameover=Gameover()
-
-
+        if snake.snake_segment[0].distance(segment) < 10:
+            # game_is_on=False
+            # gameover=Gameover()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
